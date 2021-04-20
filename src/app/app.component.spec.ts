4deeb2a377948f8265ae9083def96c914c7e9177
Rishi Ramgolam <rishi.ramgolam@madetech.com>
2021-04-20 14:35:58 +0100

    2nd part of tutorial

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {createTokenForReference} from '@angular/compiler/src/identifiers';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
