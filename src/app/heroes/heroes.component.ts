540a9082f21625b1832f891cf488ac49fc668f64
Rishi Ramgolam <rishi.ramgolam@madetech.com>
2021-04-23 15:05:18 +0100

    Added routing to the app. Separated out hero detail component.

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -13,14 +13,14 @@ export class HeroesComponent implements OnInit {
 
   heroes: Hero[] = [];
 
-  selectedHero?: Hero;
+  // selectedHero?: Hero;
 
   constructor(private heroService: HeroService, private messageService: MessageService) {}
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
+  // onSelect(hero: Hero): void {
+  //   this.selectedHero = hero;
+  //   this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  // }
 
   getHeroes(): void {
     this.heroService.getHeroes()
